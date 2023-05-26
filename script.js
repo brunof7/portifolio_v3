2053bfee3910980f630cafe6c1411df80a5a543c
Bruno Florêncio Alves <bruno.f7@hotmail.com>
2023-05-26 00:57:28 -0300

    Acrescentei o dashboard e comecei a responsavidade

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 document.querySelector(".hamburguer").addEventListener("click", ()=>
     document.querySelector(".container").classList.toggle("showMenu"));
+
+ document.querySelector(".Container-btn").addEventListener("click", ()=>
+    document.querySelector(".container").classList.toggle("showMenu"));
